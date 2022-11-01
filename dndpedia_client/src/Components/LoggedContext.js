4f37c05f00fbb8207ecae4712ec0da87ba0dda89
Bambam320 <igor.mestechkin@gmail.com>
2022-11-01 00:03:31 -0400

    updated spells

--- dndpedia_client/src/Components/LoggedContext.js
+++ dndpedia_client/src/Components/LoggedContext.js
@@ -1,3 +1,5 @@
+// imports create context hook from react
 import { createContext } from "react";
 
+// exports the LoggedContext variable for use in components
 export const LoggedContext = createContext('');
--- dndpedia_client/src/Components/Spells.js
+++ dndpedia_client/src/Components/Spells.js
@@ -1,7 +1,8 @@
-import React, { useState, useContext } from "react";
-import { useNavigate } from 'react-router-dom';
+// functional imports
+import React, { useState, useContext, useEffect } from "react";
 import { LoggedContext } from "./LoggedContext";
 
+// material ui imports
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
@@ -12,12 +13,15 @@ import Container from "@material-ui/core/Container";
 import Typography from '@material-ui/core/Typography';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
-import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 
 function Spells() {
+
+  // grabs context from LoggedContext 
   const { myFighter, spells, setSpells, characters, setCharacters } = useContext(LoggedContext)
+  
+  // sets defaultValues variable to hold an object containing keys that will be used to create the input fields for the spell
   const defaultValues = {
     name: "",
     description: "",
@@ -30,13 +34,23 @@ function Spells() {
     character_id: 0
   };
 
-  console.log('spells from spells on render', spells)
-
+  // sets a local state hook to hold the formValues from the user inputs. formValues is set to the default value
   const [formValues, setFormValues] = useState(defaultValues);
-  const [addToCharacter, setAddToCharacter] = useState(false);
-  const [chosenSpell, setChosenSpell] = useState('');
-  const navigate = useNavigate();
 
+  // holds state with logic that sets to true if a valid character is selected as champion and the user selects the spell to be added to the character
+  const [addToCharacter, setAddToCharacter] = useState(false);
+
+  // sets the local state with the spell chosen from the select menu, initially it is set to have a key of id equal to an empty string so that the select element
+  // can provide a default value to the list
+  const [chosenSpell, setChosenSpell] = useState({id: ''});
+
+  // select holds a boolean that controls the spell select drop down menu, if the spell form has been used, then the select menu is disabled
+  const [select, setSelect] = useState(false);
+
+  // textField holds a boolean that controls the spell form, if a spell has been seleced, the forms are disabled
+  const [textField, setTextField] = useState(false);
+
+  // Takes in the user input to the text fields for creating a spell, it handles numbers and strings appropriately and controls the form by updating formValues
   const handleInputChange = (e) => {
     let { name, value } = e.target;
     if (name === 'level' || name === 'damage') {
@@ -50,7 +64,8 @@ function Spells() {
     });
   };
 
-  // This changes the boolean for the addToCharacter switch and adds character id to formvalues
+  // This changes the boolean for the addToCharacter state switch and adds character id to formvalues if a character is available and returns an error if the 
+  // character is not available
   const handleAddCharacterSwitchChange = (e) => {
     console.log(myFighter.card.id)
     if (Object.keys(myFighter.card).length > 1) {
@@ -61,6 +76,18 @@ function Spells() {
     }
   }
 
+  // effect is run every time the controlled form is changed and if their are any input values in the form, then the select state is set to true, this select state
+  // will disable the select element below and prevent it from being used
+  useEffect(() => {
+    let isSelected = false
+    for (const trait in formValues) {
+      if (Boolean(formValues[trait])) {
+        isSelected = true
+      }
+    }
+    setSelect(isSelected)
+  }, [defaultValues])
+
   // give this a url with an id of the character and handle all this logic in ruby, just return the new set of spells and characters and update state
   // this is what nancy meant by handling all the work in Ruby or doing the heavy lifting
 
@@ -68,7 +95,9 @@ function Spells() {
   // otherwise let ruby read a 0 and add the spell to the spells table only, it'll update spells and characters no matter what
 
   // write the post and the patch constants, then write an if conditional, if a character is selected and a pre-existing spell is selected, run the patch,
-  // if a character is selected and a pre-existing spell is not selected, run the post. You have to add a lockout to the select menu if any of the formvalues
+  // if a character is selected and a pre-existing spell is not selected, run the post. 
+  
+  // You have to add a lockout to the select menu if any of the formvalues
   // have been changed, then you need to disable the formvalues if a spell has been selected.
   function handleSubmit(e) {
     e.preventDefault();
@@ -116,7 +145,6 @@ function Spells() {
 
   // Sets state with the selected spell from the drop down menu
   function handleSpellSelect(e) {
-    console.log('spell select from spells e.target', e.target.value)
     let thisSpell = spells.find((spell) => spell.id === e.target.value)
     setChosenSpell(thisSpell)
   }
@@ -135,6 +163,7 @@ function Spells() {
           </Grid>
           <Grid item xs={3}>
             <TextField
+              disabled={textField}
               style={{ backgroundColor: 'white' }}
               variant="filled"
               id="name"
@@ -147,6 +176,7 @@ function Spells() {
           </Grid>
           <Grid item xs={3}>
             <TextField
+              disabled={textField}
               style={{ backgroundColor: 'white' }}
               variant="filled"
               id="description"
@@ -159,6 +189,7 @@ function Spells() {
           </Grid>
           <Grid item xs={3}>
             <TextField
+              disabled={textField}
               style={{ backgroundColor: 'white' }}
               variant="filled"
               id="material"
@@ -171,6 +202,7 @@ function Spells() {
           </Grid>
           <Grid item xs={3}>
             <TextField
+              disabled={textField}
               style={{ backgroundColor: 'white' }}
               variant="filled"
               id="range"
@@ -183,6 +215,7 @@ function Spells() {
           </Grid>
           <Grid item xs={3}>
             <TextField
+              disabled={textField}
               style={{ backgroundColor: 'white' }}
               variant="filled"
               id="duration"
@@ -195,6 +228,7 @@ function Spells() {
           </Grid>
           <Grid item xs={3}>
             <TextField
+              disabled={textField}
               style={{ backgroundColor: 'white' }}
               variant="filled"
               id="casting_time"
@@ -207,6 +241,7 @@ function Spells() {
           </Grid>
           <Grid item xs={3}>
             <TextField
+              disabled={textField}
               style={{ backgroundColor: 'white' }}
               variant="filled"
               id="level"
@@ -219,6 +254,7 @@ function Spells() {
           </Grid>
           <Grid item xs={3}>
             <TextField
+              disabled={textField}
               style={{ backgroundColor: 'white' }}
               variant="filled"
               id="damage"
@@ -233,33 +269,19 @@ function Spells() {
             <Typography variant='h4' style={{ fontWeight: 'bold', textAlign: 'center', marginTop: '0px', color: '#ea2424' }}>
               Choose an existing spell!
             </Typography>
-
-
-
-
-
-
-
             <FormControl variant="outlined" style={{minWidth: 600}}>
               <InputLabel id="spell-select">Select A Spell</InputLabel>
               <Select
+                disabled={select}
                 labelId="spell-select"
                 id="spell-select"
-                value={chosenSpell}
+                value={chosenSpell.id}
                 onChange={handleSpellSelect}
                 label="chosenSpell"
-                name="spell"
               >
               {listSpells}
               </Select>
             </FormControl>
-
-
-
-
-
-
-
           </Grid>
         </Grid>
         <Grid container justifyContent="center">
